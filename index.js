8f8d1e0862cdfb6694bb0c66dd15529f79d86331
Nathan Wlotko <n8wlotko@gmail.com>
2022-06-16 23:25:53 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+
+  }
+  const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(name, event){
+    let cards = []
+    for ( let i = 0; i < name.length; i++) {
+        cards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards 
+}
+function countDown(firstNum){
+    while (firstNum > 0) {
+        console.log( firstNum );
+        firstNum -= 1;
+    }
+    console.log( firstNum);
+}
